62e0f86711bc08c56c85c1c2e7a49611972bd05d
Manuel Montenegro <manuel@nucypher.com>
2026-01-20 00:56:41 +0100

    Get wallet balance instead of channel one

--- backend/routes.ts
+++ backend/routes.ts
@@ -3,15 +3,14 @@ import { Request, Response } from "express";
 
 export const getInfo = async (req: Request, res: Response) => {
     try {
-        console.log(req.query);
         const rpcClient = await createLnRpc({
             server: req.query.server as string,
             cert: Buffer.from(req.query.cert as string, "hex").toString("utf-8"),
             macaroon: req.query.macaroon as string,
         });
-        const { alias, identityPubkey } = await rpcClient.getInfo();
-        const { balance } = await rpcClient.channelBalance();
-        res.send({ alias, identityPubkey, balance });
+        const { alias } = await rpcClient.getInfo();
+        const { totalBalance } = await rpcClient.walletBalance();
+        res.send({ alias, balance: totalBalance });
     } catch (err) {
         console.log("Error:", err);
         throw err;
